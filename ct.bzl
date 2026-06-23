omnicate/rules_erlang

commit 228b0625ece307240d34822c3f76d3ef56954e56

Fix testing support (ct, eunit)

ct.bzl
@@ -5,12 +5,16 @@ load(
     _sanitize_sname = "sanitize_sname",
 )
 load(
-    ":erlang_bytecode.bzl",
-    "erlang_bytecode",
+    "//private:util.bzl",
+    _additional_file_dest_relative_path = "additional_file_dest_relative_path",
 )
 load(
     ":erlang_app.bzl",
     "DEFAULT_TEST_ERLC_OPTS",
+)
+load(
+    ":erlang_bytecode.bzl",
+    "erlang_bytecode",
 )
 
 def additional_file_dest_relative_path(dep_label, f):
@@ -95,7 +99,7 @@ def ct_test(
     if suite_name == None or suite_name == "":
         suite_name = name
     if compiled_suites == None:
-        compiled_suites = [":{}_beam_files".format(suite_name)]
+        compiled_suites = []
     _ct_test(
         name = name,
         suite_name = suite_name,

private/ct.bzl
@@ -8,16 +8,6 @@ load(
     "flat_deps",
 )
 load(
-    ":util.bzl",
-    "erl_libs_contents",
-    "to_erlang_string_list",
-)
-load(
-    ":eunit.bzl",
-    "package_relative_dirnames",
-    "short_dirname",
-)
-load(
     "//:util.bzl",
     "path_join",
     "windows_path",
@@ -26,6 +16,16 @@ load(
     "//tools:erlang_toolchain.bzl",
     "erlang_dirs",
     "maybe_install_erlang",
+)
+load(
+    ":eunit.bzl",
+    "package_relative_dirnames",
+    "short_dirname",
+)
+load(
+    ":util.bzl",
+    "erl_libs_contents",
+    "to_erlang_string_list",
 )
 
 def sanitize_sname(s):
@@ -67,7 +67,7 @@ def _impl(ctx):
 
     erl_libs_files = erl_libs_contents(
         ctx,
-        deps = flat_deps(ctx.attr.deps),
+        deps = flat_deps(ctx.attr.deps + ctx.attr.compiled_suites),
         ez_deps = ctx.files.ez_deps,
         dir = erl_libs_dir,
     )
@@ -184,7 +184,7 @@ set -x
     -no_auto_compile \\
     -noinput \\
     ${{FILTER}} \\
-    -dir test {pa_args} \\
+    -dir ebin {pa_args} \\
     -logdir "{log_dir}" \\
     -hidden \\
     -sname {sname} ${{COVER_ARGS}} {extra_args}
@@ -271,7 +271,7 @@ echo on
     -no_auto_compile ^
     -noinput ^
     %FILTER% ^
-    -dir test {pa_args} ^
+    -dir ebin {pa_args} ^
     -logdir {drive_letter}: ^
     -hidden ^
     -sname {sname} {extra_args}

erlang_app_info.bzl
@@ -5,9 +5,12 @@ ErlangAppInfo = provider(
         "extra_apps": "Extra applications in the applications key of the .app file",
         "include": "Public header files",
         "beam": "Compiled bytecode (.beam) files, or a single ebin directory",
+        "test_beam": "Compiled test suites",
         "priv": "Additional files",
         "license_files": "License files",
         "srcs": "Source files",
+        "test_srcs": "Test source files",
+        "test_data": "Test data files",
         "deps": "Runtime dependencies of the compiled sources",
         "direct_deps": "Declared direct deps of the app",
     },
@@ -45,9 +48,12 @@ def _impl(ctx):
             extra_apps = ctx.attr.extra_apps,
             include = ctx.files.hdrs,
             beam = compiled_files,
+            test_beam = ctx.files.test_beam,
             priv = ctx.files.priv,
             license_files = ctx.files.license_files,
             srcs = ctx.files.srcs,
+            test_srcs = ctx.files.test_srcs,
+            test_data = ctx.files.test_data,
             deps = deps,
             direct_deps = ctx.attr.deps,
         ),
@@ -65,9 +71,12 @@ erlang_app_info = rule(
         "hdrs": attr.label_list(allow_files = True),
         "app": attr.label(allow_files = [".app"]),
         "beam": attr.label_list(allow_files = [".beam", ".appup"]),
+        "test_beam": attr.label_list(allow_files = [".beam"]),
         "priv": attr.label_list(allow_files = True),
         "license_files": attr.label_list(allow_files = True),
         "srcs": attr.label_list(allow_files = True),
+        "test_srcs": attr.label_list(allow_files = True),
+        "test_data": attr.label_list(allow_files = True),
         "deps": attr.label_list(providers = [ErlangAppInfo]),
     },
     provides = [ErlangAppInfo],

erlang_app_sources.bzl
@@ -9,6 +9,8 @@ def erlang_app_sources(
         public_hdrs = None,
         private_hdrs = None,
         srcs = None,
+        test_srcs = None,
+        test_data = None,
         priv = None,
         license_files = None,
         **kwargs):
@@ -32,6 +34,21 @@ def erlang_app_sources(
             "src/**/*.erl",
         ])
 
+    if test_srcs == None:
+        test_srcs = native.glob(
+            include = [
+                "test/**/*.erl",
+            ],
+            exclude = [
+                "test/data/**/*",
+            ],
+        )
+
+    if test_data == None:
+        test_data = native.glob([
+            "test/data/**/*",
+        ])
+
     if priv == None:
         priv = native.glob(
             ["priv/**/*"],
@@ -49,6 +66,8 @@ def erlang_app_sources(
         public_hdrs = public_hdrs,
         private_hdrs = private_hdrs,
         srcs = srcs,
+        test_srcs = test_srcs,
+        test_data = test_data,
         priv = priv,
         app_dir = ".",
         license_files = license_files,

erlang_autodetect.bzl
@@ -1,11 +1,11 @@
+load("@rules_erlang//:erlang_app_sources.bzl", "erlang_app_sources")
+load("@rules_erlang//:erlc_opts_file.bzl", "erlc_opts_file")
 load(
     "@rules_erlang//compat:erlang_mk.bzl",
     "app_src_from_erlang_mk_makefile",
     "erlc_opts_from_erlang_mk_makefile",
 )
 load("@rules_erlang//compat:rebar.bzl", "erlc_opts_from_rebar_config")
-load("@rules_erlang//:erlc_opts_file.bzl", "erlc_opts_file")
-load("@rules_erlang//:erlang_app_sources.bzl", "erlang_app_sources")
 
 def erlang_autodetect(name = None, testonly = False):
     app_src = None
@@ -51,5 +51,7 @@ def erlang_autodetect(name = None, testonly = False):
         app_src = app_src,
         erlc_opts_file = ":erlc_opts_file",
         testonly = testonly,
+        test_srcs = [],
+        test_data = [],
         visibility = ["//visibility:public"],
     )

private/compile_many.bzl
@@ -30,7 +30,8 @@ def _impl(ctx):
         source_info = app[ErlangAppSourcesInfo]
         compiler_srcs = (source_info.public_hdrs +
                          source_info.private_hdrs +
-                         source_info.srcs)
+                         source_info.srcs +
+                         source_info.test_srcs)
         compiler_flags.targets[source_info.app_name] = {
             "src_path": path_join(app.label.workspace_root, app.label.package),
             "erlc_opts_file": source_info.erlc_opts_file.path,
@@ -41,11 +42,13 @@ def _impl(ctx):
         apps_inputs.extend(source_info.public_hdrs)
         apps_inputs.extend(source_info.private_hdrs)
         apps_inputs.extend(source_info.srcs)
+        apps_inputs.extend(source_info.test_srcs)
         apps_inputs.append(source_info.app_src)
         apps_inputs.append(source_info.erlc_opts_file)
 
         app_outs = []
-        for src in source_info.public_hdrs + source_info.private_hdrs + source_info.srcs:
+        for src in source_info.public_hdrs + source_info.private_hdrs + \
+                   source_info.srcs + source_info.test_srcs:
             if src.basename.endswith(".erl"):
                 module_name = src.basename.removesuffix(".erl")
                 out = ctx.actions.declare_file(path_join(
@@ -74,6 +77,8 @@ def _impl(ctx):
             o.path
             for o in app_outs
         ]
+        test_modules = [src.basename.removesuffix(".erl") for src in source_info.test_srcs]
+        compiler_flags.targets[source_info.app_name]["test_modules"] = test_modules
         apps[source_info.app_name] = struct(
             source_info = source_info,
             outs = app_outs,

private/erlang_app_sources.bzl
@@ -8,6 +8,8 @@ ErlangAppSourcesInfo = provider(
         "public_hdrs": "Public header files",
         "private_hdrs": "Private header files",
         "srcs": "Source files",
+        "test_srcs": "Test source files",
+        "test_data": "Test data files",
         # "beam": "Compiled bytecode (.beam) files, or a single ebin directory",
         "priv": "Additional files",
         "app_dir": "Root directory of the application",
@@ -41,6 +43,8 @@ def _impl(ctx):
             srcs = srcs.to_list(),
             priv = ctx.files.priv,
             app_dir = ctx.file.app_dir,
+            test_srcs = ctx.files.test_srcs,
+            test_data = ctx.files.test_data,
             license_files = ctx.files.license_files,
             # precompiled_beam = ctx.files.precompiled_beam,
         ),
@@ -81,6 +85,12 @@ erlang_app_sources = rule(
         ),
         "app_dir": attr.label(
             allow_single_file = True,
+        ),
+        "test_srcs": attr.label_list(
+            allow_files = [".erl"],
+        ),
+        "test_data": attr.label_list(
+            allow_files = True,
         ),
         "generated_srcs": attr.label_list(
             providers = [ErlangGeneratedCodeInfo],

private/eunit.bzl
@@ -8,14 +8,14 @@ load(
     "windows_path",
 )
 load(
-    ":util.bzl",
-    "erl_libs_contents",
-    "to_erlang_string_list",
-)
-load(
     "//tools:erlang_toolchain.bzl",
     "erlang_dirs",
     "maybe_install_erlang",
+)
+load(
+    ":util.bzl",
+    "erl_libs_contents",
+    "to_erlang_string_list",
 )
 
 def short_dirname(f):
@@ -49,25 +49,15 @@ def _quote(string_list_term):
     return string_list_term.replace('"', '\\"')
 
 def _impl(ctx):
-    if ctx.attr.eunit_mods == [] and ctx.attr.target == None:
-        fail("Either eunit_mods or target must be set")
-    if ctx.attr.eunit_mods != [] and ctx.attr.target != None:
-        fail("eunit_mods and target cannot be set simultaneously")
-
     deps = list(ctx.attr.deps)
-    eunit_mods = list(ctx.attr.eunit_mods)
-    if ctx.attr.target != None:
-        lib_info = ctx.attr.target[ErlangAppInfo]
-        deps.extend(lib_info.deps)
-        deps.append(ctx.attr.target)
-        for m in lib_info.beam:
-            if m.extension == "beam":
-                module_name = m.basename.removesuffix(".beam")
-                if not module_name.endswith("_tests"):
-                    eunit_mods.append(module_name)
-        for s in ctx.files.compiled_suites:
-            module_name = s.basename.removesuffix(".beam")
-            if not module_name.endswith("_tests"):
+    eunit_mods = []
+    lib_info = ctx.attr.target[ErlangAppInfo]
+    deps.extend(lib_info.deps)
+    deps.append(ctx.attr.target)
+    for m in lib_info.test_beam:
+        if m.extension == "beam":
+            module_name = m.basename.removesuffix(".beam")
+            if module_name.endswith("_tests"):
                 eunit_mods.append(module_name)
 
     erl_libs_dir = ctx.label.name + "_deps"
@@ -82,10 +72,6 @@ def _impl(ctx):
     package = ctx.label.package
 
     erl_libs_path = path_join(package, erl_libs_dir)
-
-    pa_args = []
-    for dir in package_relative_dirnames(package, ctx.files.compiled_suites):
-        pa_args.extend(["-pa", dir])
 
     apps_ebin_dirs = []
     if ctx.configuration.coverage_enabled:
@@ -137,7 +123,7 @@ if [ -n "${{COVERAGE}}" ]; then
 fi
 set -x
 "{erlang_home}"/bin/erl +A1 -noinput -boot no_dot_erlang \\
-    {pa_args} {extra_args} \\
+    {extra_args} \\
     -eval "${{COVER_PRE}}case eunit:test({eunit_mods_term},{eunit_opts_term}) of ok -> ok; error -> halt(2) end, ${{COVER_POST}}halt()."
 set +x
 if [ -n "${{COVERAGE}}" ]; then
@@ -153,7 +139,6 @@ fi
             erl_libs_path = erl_libs_path if len(erl_libs_files) > 0 else "",
             package = package,
             coverdata_to_lcov = coverdata_to_lcov_path,
-            pa_args = " ".join(pa_args),
             extra_args = " ".join(ctx.attr.erl_extra_args),
             eunit_mods_term = _to_atom_list(eunit_mods),
             eunit_opts_term = eunit_opts_term,
@@ -178,13 +163,12 @@ if NOT [{package}] == [] cd {package}
 
 echo on
 "{erlang_home}\\bin\\erl" +A1 -noinput -boot no_dot_erlang ^
-    {pa_args} {extra_args} ^
+    {extra_args} ^
     -eval "case eunit:test({eunit_mods_term},{eunit_opts_term}) of ok -> ok; error -> halt(2) end, halt()" || exit /b 1
 """.format(
             package = package,
             erlang_home = windows_path(erlang_home),
             erl_libs_path = erl_libs_path if len(erl_libs_files) > 0 else "",
-            pa_args = " ".join(pa_args),
             extra_args = " ".join(ctx.attr.erl_extra_args),
             eunit_mods_term = _to_atom_list(eunit_mods),
             eunit_opts_term = eunit_opts_term,
@@ -198,7 +182,7 @@ echo on
 
     runfiles = runfiles.merge_all(
         [ctx.runfiles(
-            files = ctx.files.compiled_suites + ctx.files.data,
+            files = lib_info.test_data,
             transitive_files = depset(erl_libs_files),
         )] + ([coverdata_to_lcov[DefaultInfo].default_runfiles] if ctx.configuration.coverage_enabled else []) + [
             tool[DefaultInfo].default_runfiles
@@ -221,14 +205,10 @@ eunit_test = rule(
             cfg = "target",
         ),
         "is_windows": attr.bool(mandatory = True),
-        "compiled_suites": attr.label_list(
-            allow_files = [".beam"],
-        ),
         "eunit_mods": attr.string_list(),
         "target": attr.label(providers = [ErlangAppInfo]),
         "erl_extra_args": attr.string_list(),
         "eunit_opts": attr.string_list(),
-        "data": attr.label_list(allow_files = True),
         "deps": attr.label_list(providers = [ErlangAppInfo]),
         "ez_deps": attr.label_list(
             allow_files = [".ez"],

commit 8faae61a06455e5f49bd5734f07c1b55154f4618

Resurrect CI: pin Bazel 8.4.2, fix glob/sh_test defaults, fix CT/test regressions
The repository's GitHub Actions workflow had been failing for some time due
to a combination of Bazel version drift (last_rc -> 9.1.1rc1), breaking
changes in Bazel's glob and sh_test defaults, and regressions in the rule
implementations and test data.

Infrastructure (Bazel 8.4.2 + stricter glob/sh_test defaults):
- .bazelversion: last_rc -> 8.4.2
- erlang_app.bzl: allow_empty=True on 9 native.glob sites (include/, etc.)
- ct.bzl: allow_empty=True on the 2 globs in ct_suite/ct_suite_variant
- test/MODULE.bazel: platforms 0.0.7 -> 0.0.11; add rules_shell 0.6.1
- test/app_file/BUILD.bazel: switch app_file to public load; load sh_test
  from @rules_shell (no longer in @bazel_tools by default)

CT/eunit regressions introduced in 228b062 "Fix testing support (ct, eunit)":
- private/ct.bzl: flat_deps(deps + compiled_suites) -> flat_deps(deps);
  compiled_suites are .beam files, not ErlangAppInfo providers, so passing
  them through flat_deps causes an analysis-time error
- private/ct.bzl: ct_run -dir ebin -> -dir test; the ct_suite macro still
  compiles SUITEs to dest="test", so -dir ebin found no suites

Test data updates for product changes:
- test/shard_suite/BUILD.bazel: erlang_bytecode2 -> erlang_bytecode v1
  (v1 accepts string-list erlc_opts); replace eunit(compiled_suites=...)
  (no such attr) with test_beam=[...] on test_erlang_app + eunit_mods
- test/rules_erlang_compiler/test/dot_app_file_SUITE.erl: add
  test_modules => [] to the Target map (228b062 made the key mandatory
  in dot_app_file:render/3 but did not update the test)

Product bug fix:
- tools/rebar_config_to_erlc_opts/src/rebar_config_to_erlc_opts.erl:
  honor no_debug_info in rebar.config erl_opts by dropping the default
  +debug_info from the output when no_debug_info is present; also
  lists:flatten the io_lib:format result so output entries are flat
  strings rather than nested iolists like [[43],"deterministic"]

.bazelversion
@@ -1,1 +1,1 @@
-last_rc
+8.4.2

ct.bzl
@@ -40,7 +40,7 @@ def ct_suite(
 
     erlang_bytecode(
         name = "{}_beam_files".format(suite_name),
-        hdrs = native.glob(["include/*.hrl", "src/*.hrl"] + additional_hdrs),
+        hdrs = native.glob(["include/*.hrl", "src/*.hrl"] + additional_hdrs, allow_empty = True),
         srcs = ["test/{}.erl".format(suite_name)] + additional_srcs,
         erlc_opts = erlc_opts,
         dest = "test",
@@ -69,7 +69,7 @@ def ct_suite_variant(
     if suite_name == "":
         suite_name = name
 
-    data_dir_files = native.glob(["test/{}_data/**/*".format(suite_name)])
+    data_dir_files = native.glob(["test/{}_data/**/*".format(suite_name)], allow_empty = True)
 
     _ct_test(
         shard_suite = Label("@rules_erlang//tools/shard_suite:shard_suite"),

erlang_app.bzl
@@ -97,14 +97,17 @@ def _erlang_app(
         only_srcs = native.glob(
             ["src/**/*.erl"],
             exclude = extra_srcs,
+            allow_empty = True,
         ) + extra_srcs
         private_hdrs = native.glob(
             ["src/**/*.hrl"],
             exclude = extra_hdrs,
+            allow_empty = True,
         )
         public_hdrs = native.glob(
             ["include/**/*.hrl"],
             exclude = extra_hdrs,
+            allow_empty = True,
         ) + extra_hdrs
 
         srcs = only_srcs + private_hdrs + public_hdrs
@@ -123,7 +126,7 @@ def _erlang_app(
 
         beam_files = [":beam_files" if not test else ":test_beam_files"]
 
-    if len(native.glob(["ebin/{}.app".format(app_name)])) == 0:
+    if len(native.glob(["ebin/{}.app".format(app_name)], allow_empty = True)) == 0:
         if not test:
             app_file(
                 name = "app_file",
@@ -135,7 +138,7 @@ def _erlang_app(
                 app_env = app_env,
                 app_extra_keys = app_extra_keys,
                 extra_apps = extra_apps,
-                app_src = native.glob(["src/{}.app.src".format(app_name)]),
+                app_src = native.glob(["src/{}.app.src".format(app_name)], allow_empty = True),
                 modules = beam_files,
                 deps = deps + runtime_deps,
                 dest = "ebin",
@@ -146,9 +149,9 @@ def _erlang_app(
     else:
         app = "ebin/{}.app".format(app_name)
 
-    if len(native.glob(["ebin/{}.appup".format(app_name)])) > 0:
+    if len(native.glob(["ebin/{}.appup".format(app_name)], allow_empty = True)) > 0:
         appup = "ebin/{}.appup".format(app_name)
-    elif len(native.glob(["src/{}.appup".format(app_name)])) > 0:
+    elif len(native.glob(["src/{}.appup".format(app_name)], allow_empty = True)) > 0:
         if not test:
             copy_file(
                 name = "appup",
@@ -165,12 +168,14 @@ def _erlang_app(
         priv = native.glob(
             ["priv/**/*"],
             exclude = extra_priv,
+            allow_empty = True,
         ) + extra_priv
 
     if license_files == None:
         license_files = native.glob(
             ["LICENSE*"],
             exclude = extra_license_files,
+            allow_empty = True,
         ) + extra_license_files
 
     erlang_app_info(

private/ct.bzl
@@ -63,7 +63,7 @@ def _impl(ctx):
 
     erl_libs_files = erl_libs_contents(
         ctx,
-        deps = flat_deps(ctx.attr.deps + ctx.attr.compiled_suites),
+        deps = flat_deps(ctx.attr.deps),
         ez_deps = ctx.files.ez_deps,
         dir = erl_libs_dir,
     )
@@ -179,7 +179,7 @@ set -x
     -no_auto_compile \\
     -noinput \\
     ${{FILTER}} \\
-    -dir ebin {pa_args} \\
+    -dir test {pa_args} \\
     -logdir "{log_dir}" \\
     -hidden \\
     -sname {sname} ${{COVER_ARGS}} {extra_args}

tools/rebar_config_to_erlc_opts/src/rebar_config_to_erlc_opts.erl
@@ -21,13 +21,18 @@ erlc_opts(RebarConfig) ->
                 lists:flatmap(
                     fun
                         (#{value := V, kind := erlc}) ->
-                            [io_lib:format("+~s", [V])];
+                            [lists:flatten(io_lib:format("+~s", [V]))];
                         (_) ->
                             []
                     end,
                     ErlOpts
                 );
             _ ->
-                ["+debug_info"]
+                []
         end,
-    lists:delete("+warnings_as_errors", lists:usort(["+deterministic", "+debug_info" | ErlcOpts])).
+    Defaults =
+        case lists:member("+no_debug_info", ErlcOpts) of
+            true -> ["+deterministic"];
+            false -> ["+deterministic", "+debug_info"]
+        end,
+    lists:delete("+warnings_as_errors", lists:usort(Defaults ++ ErlcOpts)).

test/MODULE.bazel
@@ -5,11 +5,15 @@ module(
 
 bazel_dep(
     name = "platforms",
-    version = "0.0.7",
+    version = "0.0.11",
 )
 bazel_dep(
     name = "rules_erlang",
     version = "3.0.0",
+)
+bazel_dep(
+    name = "rules_shell",
+    version = "0.6.1",
 )
 local_path_override(
     module_name = "rules_erlang",

test/app_file/BUILD.bazel
@@ -1,4 +1,5 @@
-load("@rules_erlang//private:app_file.bzl", "app_file")
+load("@rules_erlang//:app_file.bzl", "app_file")
+load("@rules_shell//shell:sh_test.bzl", "sh_test")
 
 app_file(
     name = "app_file",

test/rules_erlang_compiler/test/dot_app_file_SUITE.erl
@@ -32,7 +32,8 @@ injects_modules(Config) ->
             "bazel-out/darwin-fastbuild/bin/deps_dir/basic/src/basic_acceptor.erl",
             "bazel-out/darwin-fastbuild/bin/deps_dir/basic/ebin/basic_acceptors_sup.beam",
             "bazel-out/darwin-fastbuild/bin/deps_dir/basic/src/basic_acceptors_sup.erl"
-        ]
+        ],
+        test_modules => []
     },
 
     Output = filename:join([DestDir, "basic", "ebin", "basic.app"]),

test/shard_suite/BUILD.bazel
@@ -1,9 +1,9 @@
 load(
-    "@rules_erlang//private:app_file.bzl",
+    "@rules_erlang//:app_file.bzl",
     "app_file",
 )
 load(
-    "@rules_erlang//:erlang_bytecode2.bzl",
+    "@rules_erlang//:erlang_bytecode.bzl",
     "erlang_bytecode",
 )
 load(
@@ -69,29 +69,6 @@ erlang_app_info(
     beam = [":beam_files"],
 )
 
-erlang_app_info(
-    name = "test_erlang_app",
-    testonly = True,
-    srcs = ["@rules_erlang//tools/shard_suite:src/shard_suite.erl"],
-    app = ":app_file",
-    app_name = APP_NAME,
-    beam = [":test_beam_files"],
-)
-
-xref()
-
-plt(
-    name = "base_plt",
-    apps = DEFAULT_PLT_APPS + [
-        "crypto",
-        "common_test",
-    ],
-)
-
-dialyze(
-    plt = ":base_plt",
-)
-
 erlang_bytecode(
     name = "test_example_suite_beam",
     testonly = True,
@@ -128,11 +105,38 @@ erlang_bytecode(
     ],
 )
 
-eunit(
-    name = "eunit",
-    compiled_suites = [
+erlang_app_info(
+    name = "test_erlang_app",
+    testonly = True,
+    srcs = ["@rules_erlang//tools/shard_suite:src/shard_suite.erl"],
+    app = ":app_file",
+    app_name = APP_NAME,
+    beam = [":test_beam_files"],
+    test_beam = [
         ":test_example_suite_beam",
         ":test_shard_suite_test_beam",
+    ],
+)
+
+xref()
+
+plt(
+    name = "base_plt",
+    apps = DEFAULT_PLT_APPS + [
+        "crypto",
+        "common_test",
+    ],
+)
+
+dialyze(
+    plt = ":base_plt",
+)
+
+eunit(
+    name = "eunit",
+    eunit_mods = [
+        "example_suite",
+        "shard_suite_test",
     ],
     target = ":test_erlang_app",
 )